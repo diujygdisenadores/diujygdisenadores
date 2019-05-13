62db81915bee678142091b58667817605d0fd33c
Guillermo Bueno Vargas <Guillergood@gmail.com>
2019-05-13 16:53:03 +0200

    Terminado
    Cambios terminados

--- js/searchBehaviour.js
+++ js/searchBehaviour.js
@@ -2,6 +2,14 @@
 
 
 var eventos = ["Pesca", "Desayuno con compañeros", "Charla sobre Bootstrap", "Conferencia de HTML5"];
+var precios = [5, 3, 0, 0];
+var etiqueta = ["Deporte", "Comunidad", "Comunidad", "Comunidad"];
+var lugar = ["Pantano de Cubillas", "ETSIIT", "ETSIIT", "ETSIIT"];
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var busqueda = [];
+
+
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
     continua = true;
@@ -19,36 +27,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function Busqueda() {
     var inputTexto = document.getElementById("inputTexto").value;
-    var busqueda = [];
+    var inputFecha = document.getElementById("inputFecha").value;
+    var inputPrecio = document.getElementById("inputPrecio").value;
+    var inputLugar = document.getElementById("inputPrecio").value;
+    var d = new Date();
 
     if(inputTexto.length > 0){
-    	for(let i = 0; i < eventos.length; ++i){
-	    	if(eventos[i].toLowerCase().includes(inputTexto.toLowerCase())){
-	    		busqueda.push(eventos[i]);
-	    	}
-    	}
-
-    	var eventoNoBuscado = eventos.filter(
-		    function(e) {
-		      return this.indexOf(e) < 0;
-		    },
-		    busqueda
-		)
-
-
-    	if(eventoNoBuscado.length > 0){
-			for(let i = 0; i < eventoNoBuscado.length; ++i){
-				var content = document.getElementById(eventoNoBuscado[i]);
-				//if (content.style.display === "flex") {
-				  content.style.display = "none";
-				//}
-	    	}
-    	}
-    	else{
-    		MuestraTodos();
-    	}
+    	FiltraPorTexto(inputTexto);
     }
-    else{
+     
+    if(inputFecha.length > 0){
+
+    }
+    if(inputPrecio.length > 0){
+    	
+    }
+    if(inputLugar.length > 0){
+    	
+    }
+
+    if((inputTexto.length + inputFecha.length + inputPrecio.length + inputLugar.length) == 0){
     	MuestraTodos();
     }
 }
@@ -56,7 +54,41 @@ function Busqueda() {
 function MuestraTodos(){
 	for(let i = 0; i < eventos.length; ++i){
 		var content = document.getElementById(eventos[i]);
-		content.style.display = "flex";
-			
+		content.style.display = "flex";		
 	}	
+}
+
+function FiltraPorTexto(inputTexto){
+	for(let i = 0; i < eventos.length; ++i){
+		if(eventos[i].toLowerCase().includes(inputTexto.toLowerCase())){
+			busqueda.push(eventos[i]);
+		}
+	}
+
+	var eventoNoBuscado = eventos.filter(
+	    function(elemento) {
+	      return this.indexOf(elemento) < 0;
+	    },
+	    busqueda
+	)
+
+
+	if(eventoNoBuscado.length > 0){
+		for(let i = 0; i < eventoNoBuscado.length; ++i){
+			var content = document.getElementById(eventoNoBuscado[i]);
+		  	content.style.display = "none";
+		}
+	}
+	else{
+		MuestraTodos();
+	}
+}
+
+function filterSelection(valor) {
+  var elementos;
+  elementos = document.getElementsByClassName("etiqueta");
+  if (valor == "Ninguna") valor = "";
+  for (let i = 0; i < elementos.length; i++) {
+
+  }
 }
